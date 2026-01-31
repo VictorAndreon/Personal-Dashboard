79261b1237521eca33b5b95b1eed4bc7100bd42f
Andreonn <victorcamargoandreon@gmail.com>
2026-01-31 00:25:39 -0300

    feat: Ajustado a lógica de MovimentacaoController e modificado questões da navigation bar

--- src/app/Http/Controllers/MovimentacaoController.php
+++ src/app/Http/Controllers/MovimentacaoController.php
@@ -10,9 +10,6 @@ class MovimentacaoController extends Controller
 {
     public function index(Request $request)
     {
-        // Pega o tipo da URL (?tipo_movimentacao=entrada ou ?tipo_movimentacao=saida)
-        $tipo_movimentacao = $request->query('tipo_movimentacao', 'entrada'); 
-        
         // Busca no banco só o que é do usuário logado E do tipo selecionado
         $movimentacoes = Movimentacao::where('user_id', Auth::id())
             ->orderBy('dt_transacao', 'desc')
@@ -22,10 +19,9 @@ class MovimentacaoController extends Controller
     }
 
     // Mostra o formulário de criar
-    public function create(Request $request)
+    public function create()
     {
-        $tipo_movimentacao = $request->query('tipo_movimentacao', 'entrada');
-        return view('movimentacao.create', compact('tipo_movimentacao'));
+        return view('movimentacao.create');
     }
 
     // Salva no banco
@@ -39,25 +35,21 @@ class MovimentacaoController extends Controller
         
         // Adiciona o ID do usuário automaticamente
         $validated['user_id'] = Auth::id();
-
         Movimentacao::create($validated);
 
-
-        return redirect()->route('movimentacao.index', ['tipo_movimentacao' => $request->tipo_movimentacao]);
+        return redirect()->route('movimentacao.index');
     }
 
     public function destroy($idTransacao)
     {
         $movimentacao = Movimentacao::findOrFail($idTransacao);
-        $tipo_movimentacao = $movimentacao->tipo_movimentacao;
-
         try{
             $movimentacao->delete();
 
-            return redirect()->route('movimentacao.index', ['tipo_movimentacao' => $tipo_movimentacao])
+            return redirect()->route('movimentacao.index')
                  ->with('success', 'Registro deletado!');
         }catch(\Exception $ex){
-            return redirect()->route('movimentacao.index', ['tipo_movimentacao' => $tipo_movimentacao])
+            return redirect()->route('movimentacao.index')
                  ->with('error', 'Houve um erro ao deletar o registro!');
         }
     }
